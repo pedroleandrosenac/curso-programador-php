2c8e4cf73e05e1ebd979dbd35eef80bb269d3c17
Pedro Leandro Gomes da Silva <pedroleandrogomesdasilva@outlook.com>
2026-09-17 16:19:48 -0300

    refactor: refatorar prática de sanitização

--- 08-sanitizacao/20-sanitizacao-pratica/cadastrar.php
+++ 08-sanitizacao/20-sanitizacao-pratica/cadastrar.php
@@ -5,13 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Entrar</title>
+    <title>Cadastrar</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div style="width: 100%; max-width: 400px;">
         <div class="text-center mb-4">
@@ -20,22 +19,30 @@
 
         <form action="" method="" class="p-4 border rounded shadow-sm">
             <div class="mb-3">
+                <label for="nome_completo" class="form-label">Nome Completo</label>
+                <input type="text" id="nome_completo" name="nome_completo" class="form-control" placeholder="Digite seu nome completo" required>
+            </div>
+
+            <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" name="email" class="form-control" placeholder="Digite seu email"
-                       required>
+                <input type="email" id="email" name="email" class="form-control" placeholder="Digite seu email" required>
             </div>
 
             <div class="mb-3">
                 <label for="senha" class="form-label">Senha</label>
-                <input type="password" id="senha" name="senha" class="form-control" placeholder="Digite sua senha"
-                       required>
+                <input type="password" id="senha" name="senha" class="form-control" placeholder="Digite sua senha" required>
             </div>
 
-            <button type="submit" class="btn btn-primary w-100">Entrar</button>
+            <div class="mb-3">
+                <label for="confirmar_senha" class="form-label">Confirmar Senha</label>
+                <input type="password" id="confirmar_senha" name="confirmar_senha" class="form-control" placeholder="Confirme sua senha" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary w-100">Cadastrar</button>
         </form>
 
         <div class="text-center mt-3">
-            <a href="cadastrar.php">Ainda não tem login? Cadastre-se</a>
+            <a href="index.php">Já tem login? Voltar para o login</a>
         </div>
     </div>
 </div>
